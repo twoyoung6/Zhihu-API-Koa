8b6c3d5173c6f3d4e76a3076417bc9a03afeb878
yangzhan <yzlz5259@163.com>
2020-10-05 10:48:25 +0800

    koa-router 路由前缀；路由中传递多中间件（授权中间件）的使用

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const app = new Koa()
 const router = new Router()
+const userRouter = new Router({ prefix: '/users' }) // koa-router 路由前缀
+
+// 路由中传递多中间件（授权中间件）
+const auth = async (ctx, next) => {
+  if (ctx.url !== 'users') {
+    await ctx.throw(401)
+  }
+}
 
 router.get('/', async (ctx, next) => {
-  console.log('哈哈')
+  ctx.body = 'hello koa'
+})
+
+userRouter.get('/', auth, async (ctx, next) => {
+  ctx.body = '用户'
 })
 
 app.use(router.routes())
+app.use(userRouter.routes())
 
 app.listen(3000)
 
