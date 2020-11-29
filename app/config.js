50b45cdc4fd2c4038d5c32696cbbc3a406977042
yangzhan <yzlz5259@163.com>
2020-11-29 16:10:46 +0800

    更新 远程数据库地址

--- app/config.js
+++ app/config.js
@@ -1,7 +1,6 @@
 // 配置文件
 module.exports = {
   // dbUrl: 'mongodb://admin:admin@localhost:27017', // 本地
-  dbUrl:
-    'mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb', // 生产
+  dbUrl: 'mongodb://admin:wdcs303@115.159.55.14:27017/test', // 生产
   secret: 'zhihu-pwd', // JWT 签名密钥
 }
