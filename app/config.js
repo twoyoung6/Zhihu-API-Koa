9b09659bf0e2e9b3d1bc70082f4fec1d7b80e0df
yangzhan <yangzhan@wondersgroup.com>
2020-12-01 16:39:37 +0800

    同上

--- app/config.js
+++ app/config.js
@@ -2,11 +2,10 @@
 const readline = require('readline-sync');
 let name = readline.question("your Database name: ");
 let password = readline.question("your Database password: ");
-console.log(name + ':' + password);
 
 module.exports = {
   // dbUrl: 'mongodb://admin:admin@localhost:27017', // 本地
   // dbUrl: 'mongodb://admin:wdcs303@115.159.55.14:27017/test', // 生产
-  dbUrl: 'mongodb://name:password@115.159.55.14:27017/test',
+  dbUrl: `mongodb://${name}:${password}@115.159.55.14:27017/test`,
   secret: 'zhihu-pwd', // JWT 签名密钥
 }
