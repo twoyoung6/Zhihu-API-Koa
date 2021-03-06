4e250bc3d190edbfa67c0ed42606c270c91bb4c2
yangzhan <yzlz5259@163.com>
2021-03-06 12:06:55 +0800

    更新 api url
    Signed-off-by: yangzhan <yzlz5259@163.com>

---
+++ app/config.default.js
@@ -0,0 +1,5 @@
+module.exports = {
+  dbUrl: 'mongodb://admin:wdcs303@115.159.55.14:27017/test', // 生产
+  dbUrl: 'mongodb://admin:admin@localhost:27017', // 本地
+  secret: 'zhihu-pwd', // JWT 签名密钥
+}
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router')
-const router = new Router({ prefix: '/api' })
-const { hello, post, upload, login } = require('../controllers/home')
+const router = new Router()
+const { hello, post, upload } = require('../controllers/home')
 const jwt = require('koa-jwt')
 const { secret } = require('../config')
 const auth = jwt({ secret })
@@ -8,10 +8,7 @@ const auth = jwt({ secret })
 router.get('/', async (ctx) => {
   ctx.redirect('login.html')
 })
-//登录路由
-router.post('/login', async (ctx, next) => {
-  await login(ctx)
-})
+
 // 首页
 router.get('/:name/:age', async (ctx, next) => {
   await hello(ctx)
